fc1e170d91062ff1d22ddf191031149eb4a23539
paipure <35395155+paipure@users.noreply.github.com>
2023-07-10 00:31:30 +0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // Drag and drop
-interact('.garbage').draggable({
+var interactDraggable;
+var interactDropzone;
+
+function initializeGame() {
+  interactDraggable = interact('.garbage').draggable({
     inertia: true,
     modifiers: [
       interact.modifiers.restrictRect({
@@ -10,72 +14,110 @@ interact('.garbage').draggable({
     autoScroll: true,
     onmove: dragMoveListener
   });
-  
+
+  interact('.garbage').on('dragend', function (event) {
+    var garbage = event.target;
+    garbage.style.transform = 'none';
+    garbage.setAttribute('data-x', '0');
+    garbage.setAttribute('data-y', '0');
+  });
+
   // Drop targets
   var recycleBin = document.getElementById('recycle-bin');
   var generalTrashBin = document.getElementById('general-trash-bin');
   var foodWasteBin = document.getElementById('food-waste-bin');
-  
-  // Counter variables
-  var score = 0;
-  var totalGarbages = 5;
-  var garbagesInCorrectBins = 0;
-  
-  // Drag move listener
-  function dragMoveListener(event) {
-    var target = event.target;
-    var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-    var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-  
-    target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-  }
-  
-  // Check if garbage is in the correct bin
-  function checkBin(garbage, binId) {
-    if (garbage.id === 'carrots' && binId === 'food-waste-bin') {
-      return true;
-    } else if (
-      (garbage.id === 'can' ||
-        garbage.id === 'plastic-bottle' ||
-        garbage.id === 'newspaper' ||
-        garbage.id === 'plastic-bag') &&
-      binId === 'recycle-bin'
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  // Drop event listeners
-  interact('.trash-bin').dropzone({
+
+  interactDropzone = interact('.trash-bin').dropzone({
     accept: '.garbage',
     ondrop: function (event) {
       var garbage = event.relatedTarget;
       var bin = event.target;
-  
+
       if (checkBin(garbage, bin.id)) {
         garbage.style.display = 'none';
         score += 10;
         garbagesInCorrectBins++;
-  
+
         if (garbagesInCorrectBins === totalGarbages) {
           endGame();
         }
       } else {
         endGame();
       }
-  
+
       document.getElementById('score').textContent = 'Score: ' + score;
     }
   });
-  
-  // Game over
-  function endGame() {
-    interact('.garbage').unset();
-    interact('.trash-bin').unset();
-    alert('Game Over! Your score is ' + score);
+}
+
+// Drag move listener
+function dragMoveListener(event) {
+  var target = event.target;
+  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+  target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+  target.setAttribute('data-x', x);
+  target.setAttribute('data-y', y);
+}
+
+// Check if garbage is in the correct bin
+function checkBin(garbage, binId) {
+  if (garbage.id === 'carrots' && binId === 'food-waste-bin') {
+    return true;
+  } else if (
+    (garbage.id === 'can' ||
+      garbage.id === 'plastic-bottle' ||
+      garbage.id === 'newspaper' ||
+      garbage.id === 'plastic-bag') &&
+    binId === 'recycle-bin'
+  ) {
+    return true;
+  } else {
+    return false;
   }
-  
+}
+
+// Counter variables
+var score = 0;
+var totalGarbages = 5;
+var garbagesInCorrectBins = 0;
+
+// Game over
+function endGame() {
+  interactDraggable.unset();
+  interactDropzone.unset();
+
+  // Reset garbage positions
+  var garbages = document.querySelectorAll('.garbage');
+  for (var i = 0; i < garbages.length; i++) {
+    garbages[i].style.transform = 'none';
+    garbages[i].setAttribute('data-x', '0');
+    garbages[i].setAttribute('data-y', '0');
+    garbages[i].style.display = 'block';
+  }
+
+  var finalScore = score;
+  score = 0;
+  garbagesInCorrectBins = 0;
+
+  document.getElementById('score').textContent = 'Score: ' + finalScore;
+
+  var popup = document.createElement('div');
+  popup.className = 'popup';
+  popup.innerHTML = 'Game Over! Your score is ' + finalScore;
+
+  var overlay = document.createElement('div');
+  overlay.className = 'overlay';
+  overlay.appendChild(popup);
+
+  document.body.appendChild(overlay);
+
+  // Close popup on click
+  overlay.addEventListener('click', function () {
+    overlay.remove();
+    initializeGame(); // Start a new game
+  });
+}
+
+initializeGame(); // Start the initial game
